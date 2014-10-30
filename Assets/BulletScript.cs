ef51c41cce112ef0d088dc7024da13125f46f833
Robert <robertmskelton@gmail.com>
2014-10-30 11:39:41 -0700

    Added a basic shooting script, but it's pretty shakey since I have to use a counter instead of some sort of deltaTime

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	float damage;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce (new Vector2 (10.0f,0));
+	}
+
+	void ApplyDamage(float d)
+	{
+		print ("damage dealt");
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		gameObject.SendMessage ("ApplyDamage", damage);
+		Destroy (other.gameObject);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public Rigidbody bullet;
+	public int shotCD;
+	private int counter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Z) && counter >= shotCD)
+		{
+			Rigidbody clone;
+			clone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+
+			counter = 0;
+		}
+		counter++;
+	}
+}
