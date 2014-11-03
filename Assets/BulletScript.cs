ee7b35a97ec6c724fd06163b7cc6d7cad6d9b805
Robert <robertmskelton@gmail.com>
2014-11-02 17:55:34 -0800

    faulty shooting mechanic needs to be polished

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -3,11 +3,34 @@ using System.Collections;
 
 public class BulletScript : MonoBehaviour {
 
-	float damage;
+	public float damage;
+	public float pushForce;
+
+	private int direction;
+
+	// constructor will be passed the direction of the player
+	// this will allow us to apply the force in the proper
+	// direction
 
 	// Use this for initialization
 	void Start () {
-		rigidbody.AddForce (new Vector2 (10.0f,0));
+		direction = GameObject.Find ("Player").GetComponent<Movement> ().facing;
+
+		switch (direction) 
+		{
+			case 1:
+				rigidbody2D.AddForce (new Vector2 (pushForce, 0));
+				break;
+			case 2:
+				rigidbody2D.AddForce (new Vector2 (0, -pushForce));
+				break;
+			case 3:
+				rigidbody2D.AddForce (new Vector2 (-pushForce, 0));
+				break;
+			case 4:
+				rigidbody2D.AddForce (new Vector2 (0, pushForce));
+				break;
+		}
 	}
 
 	void ApplyDamage(float d)
--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -3,21 +3,24 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour {
 
-	public Rigidbody bullet;
+	public GameObject bullet;
 	public int shotCD;
+
 	private int counter;
+	private Movement movement;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Z) && counter >= shotCD)
 		{
-			Rigidbody clone;
-			clone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+			// cannot convert this to rigidbody2d
+			GameObject clone;
+			clone = (GameObject) Instantiate(bullet, transform.position, Quaternion.identity);
 
 			counter = 0;
 		}
