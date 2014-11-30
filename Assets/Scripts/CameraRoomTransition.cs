3329468eeea468454fd86ff7ee2a615fff92d525
Cheddar2012 <cheddar1@outlook.com>
2014-11-30 14:00:04 -0800

    Small update to CameraRoomTransition
    Using player's Transform instead of GameObject

--- Assets/Scripts/CameraRoomTransition.cs
+++ Assets/Scripts/CameraRoomTransition.cs
@@ -8,23 +8,23 @@ public class CameraRoomTransition : MonoBehaviour {
 
 	Vector3 currentPosition;
 	//Vector3 newPosition;
-	GameObject player;
+	Transform playerTransform;
 
 	// Use this for initialization
 	void Start () {
 		currentPosition = transform.position;
-		player = GameObject.FindGameObjectWithTag("Player");
+		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player.transform.position.x > currentPosition.x + roomWidth/2) {
+		if (playerTransform.position.x > currentPosition.x + roomWidth/2) {
 			currentPosition = currentPosition + new Vector3(roomWidth,0,0);
-		} else if (player.transform.position.x < currentPosition.x - roomWidth/2) {
+		} else if (playerTransform.position.x < currentPosition.x - roomWidth/2) {
 			currentPosition = currentPosition - new Vector3(roomWidth,0,0);
-		} else if (player.transform.position.y > currentPosition.y + roomHeight/2) {
+		} else if (playerTransform.position.y > currentPosition.y + roomHeight/2) {
 			currentPosition = currentPosition + new Vector3(0,roomHeight,0);
-		} else if (player.transform.position.y < currentPosition.y - roomHeight/2) {
+		} else if (playerTransform.position.y < currentPosition.y - roomHeight/2) {
 			currentPosition = currentPosition - new Vector3(0,roomHeight,0);
 		}
 		transform.position = currentPosition;
