cc79d53d8a290526b5f4c8d8d335d2b312702841
Cheddar2012 <cheddar1@outlook.com>
2014-12-09 15:25:22 -0800

    Added health to HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+	
+	GameObject health;
+	Health healthScript;
+
+	// Use this for initialization
+	void Start () {
+		healthScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+		health = new GameObject();
+		health.AddComponent("GUIText");
+		health.transform.position = new Vector3(0.11f,0.98f,0.0f);
+		health.guiText.text = "HEALTH";
+		health.guiText.fontSize = 64;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		health.guiText.text = "" + healthScript.health;
+	}
+}
