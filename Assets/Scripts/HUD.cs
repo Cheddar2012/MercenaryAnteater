92b61442018ab335af77dce5b903b0e2141df23a
jheckard <jason.heckard@yahoo.com>
2014-12-12 13:00:54 -0800

    renamed GUI element objects in Unity scene

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -17,6 +17,7 @@ public class HUD : MonoBehaviour {
 		healthScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
 		health = new GameObject();
 		health.AddComponent("GUIText");
+		health.name = "Health GUI";
 		health.transform.position = new Vector3(0.05f,0.99f,0.0f);
 		health.guiText.text = "";
 		health.guiText.fontSize = 48;
@@ -24,12 +25,14 @@ public class HUD : MonoBehaviour {
 		ammoScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Ammo>();
 		rapid = new GameObject();
 		rapid.AddComponent("GUIText");
+		rapid.name = "Rapid Ammo GUI";
 		rapid.transform.position = new Vector3(0.4f, 0.99f, 0.0f);
 		rapid.guiText.text = "";
 		rapid.guiText.fontSize = 32;
 
 		sGun = new GameObject();
 		sGun.AddComponent("GUIText");
+		sGun.name = "sGun Ammo GUI";
 		sGun.transform.position = new Vector3(0.4f, 0.93f, 0.0f);
 		sGun.guiText.text = "";
 		sGun.guiText.fontSize = 32;
