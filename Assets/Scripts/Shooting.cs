37784a8716e0c2cfddfc4dda84d32a7fe8aee608
jheckard <jason.heckard@yahoo.com>
2014-12-11 20:35:08 -0800

    added spread gun shot to v key

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,6 +7,11 @@ public class Shooting : MonoBehaviour {
 	public float shotCD = 0.2f;	// RS: can be changed in inspector, may not be the best implementation
 	public KeyCode code = KeyCode.Z;
 
+	// JH adding code for short range shotgun
+	public KeyCode shotgun = KeyCode.V;
+	public GameObject bullet2;
+	public GameObject bullet3;
+
 	private float multiplier; 	// RS: is the distance between player and bullet's instantiation
 	private Movement player; 	// RS: gets the player's movement script for "facing"
 	private float timestamp;
@@ -44,6 +49,80 @@ public class Shooting : MonoBehaviour {
 			// RS: means next time player can shoot will be the current time + however long
 			//	the cooldown is
 			timestamp = Time.time + shotCD;
+		}
+
+		// JH created a similar if/switch shotgun attacks
+		// shotgun shoots 3 bullets, but the range is short 
+		// need to create a new prefab to be bullets that are short range 
+			if(Input.GetKeyDown (shotgun) && timestamp <= Time.time)
+		{
+			GameObject clone1;
+			GameObject clone2;
+			GameObject clone3;
+			
+			switch(player.facing)
+			{
+			case 1:
+				clone1 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + 25 * multiplier * Vector3.up , Quaternion.identity);
+				
+				clone2 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + 25 * multiplier * Vector3.up , Quaternion.identity);
+				clone2.rigidbody2D.AddForce(new Vector2
+				                            (100, 0) );
+				
+				clone3 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + 25 * multiplier * Vector3.up , Quaternion.identity);
+				clone3.rigidbody2D.AddForce(new Vector2
+				                            (-100, 0) );
+				break;
+			case 2:
+				clone1 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.left , Quaternion.identity);
+				
+				clone2 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.left , Quaternion.identity);
+				clone2.rigidbody2D.AddForce(new Vector2
+				                            (0, 100) );
+				
+				clone3 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.left , Quaternion.identity);
+				clone3.rigidbody2D.AddForce(new Vector2
+				                            (0, -100) );
+				break;
+			case 3:
+				clone1 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.down , Quaternion.identity);
+				
+				clone2 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.down , Quaternion.identity);
+				clone2.rigidbody2D.AddForce(new Vector2
+				                            (-100, 0) );
+				
+				clone3 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.down , Quaternion.identity);
+				clone3.rigidbody2D.AddForce(new Vector2
+				                            (100, 0) );
+				break;
+			case 4:
+				clone1 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.right , Quaternion.identity);
+				
+				clone2 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.right , Quaternion.identity);
+				clone2.rigidbody2D.AddForce(new Vector2
+				                            (0, -100) );
+				
+				clone3 = (GameObject) Instantiate(bullet, 
+				                                  transform.position + multiplier * Vector3.right , Quaternion.identity);
+				clone3.rigidbody2D.AddForce(new Vector2
+				                            (0, 100) );
+				break;
+			}
+			
+			// RS: means next time player can shoot will be the current time + however long
+			//	the cooldown is
+				timestamp = Time.time + shotCD;
 		}
 	}
 }
