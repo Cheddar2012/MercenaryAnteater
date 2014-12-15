d448c5d8943f402d8905e0d19e66b4a378114467
Cheddar2012 <cheddar1@outlook.com>
2014-12-14 19:01:17 -0800

    Bush viper now has attack animation, albeit buggy

---
+++ Assets/Scripts/SnakeAnimator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeAnimator : MonoBehaviour {
+
+	public Sprite[] animals;
+	public SpriteRenderer renderer;
+	public int spriteToShow;
+	public GameObject player;
+
+	public SnakeAttack sAttack;
+	public float attackTime = 0.3f;
+
+	public int direction;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		renderer = (SpriteRenderer)GetComponent("SpriteRenderer");
+		sAttack = (SnakeAttack)GetComponent("SnakeAttack");
+		direction = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!sAttack.attacking)
+		{
+			// facing left
+			if( (player.transform.position.x - transform.position.x < 0) )
+			{
+				direction = 2;
+				renderer.sprite = animals[0];
+			}
+			
+			// facing right
+			else
+			{
+				direction = 4;
+				renderer.sprite = animals[2];
+			}
+		}
+
+		else
+		{
+			if(direction == 2)
+			{
+				renderer.sprite = animals[1];
+			}
+			
+			else
+			{
+				renderer.sprite = animals[3];
+			}
+			
+			attackTime = Time.time + attackTime;
+		}
+	}
+}
--- Assets/Scripts/SnakeAttack.cs
+++ Assets/Scripts/SnakeAttack.cs
@@ -3,31 +3,62 @@ using System.Collections;
 
 public class SnakeAttack : MonoBehaviour {
 
+	public int facing{ get; private set; }
+
+	public EnemyMovement em; 			
+	public GameObject player;
+
+	public bool attacking;
+	public float attackTime = 0.4f;
+	public float stopAttack;
+
 	public int damage = 1;				// RS: damage to deal per second
 	public float attackCD = 3;			// RS: seconds between poison resets
 	public float attackRange = 50;		// RS: range to apply poision
 	public float poisonDuration = 6;	// RS: time poison lasts (in seconds)
 	
 	private float attackTimestamp;		// RS: game time at which snake can re-poison
-	private GameObject player;			
+
 	// Use this for initialization
 	void Start () {
-		attackTimestamp = Time.time;
 		player = GameObject.Find ("Player");
+		em = GetComponent<EnemyMovement> ();
+
+		facing = 2;
+
+		attacking = false;
+		stopAttack = 0.0f;
+
+		attackTimestamp = Time.time;
 	}
 
 	// RS: if the snake can attack, tell set poision damage and tell player he is poisned
 	//	then set cooldown for attack
 	void OnCollisionStay2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Player" && Time.time >= attackTimestamp) {
-			player.BroadcastMessage("SetPoisonDamage", damage);
-			player.BroadcastMessage ("PoisonPlayer", poisonDuration);
-			attackTimestamp = Time.time + poisonDuration;
+		if (coll.gameObject.tag == "Player") {
+
+			if (Time.time >= attackTimestamp) {
+				player.BroadcastMessage("SetPoisonDamage", damage);
+				player.BroadcastMessage ("PoisonPlayer", poisonDuration);
+				attackTimestamp = Time.time + poisonDuration;
+			}
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		facing = em.facing;
+
+		if (player != null && em.playerInAggroRange() && !attacking ) 
+		{
+			attacking = true;
+			stopAttack = Time.time + attackTime;
+		}
+
+		if(Time.time > stopAttack)
+		{
+			Debug.Log("Swag");
+			attacking = false;
+		}
 	}
 }
