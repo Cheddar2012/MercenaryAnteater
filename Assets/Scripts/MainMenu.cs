0eb67553f5ac379498f0026653afedfa8e29f36c
Cheddar2012 <cheddar1@outlook.com>
2014-12-14 16:30:09 -0800

    Created basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string text;
+	public int fontSize;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent("GUIText");
+		gameObject.guiText.text = text;
+		gameObject.guiText.anchor = TextAnchor.MiddleCenter;
+		gameObject.guiText.alignment = TextAlignment.Center;
+		gameObject.guiText.fontSize = fontSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Application.LoadLevel("Scene002");
+		}
+	}
+}
