75223a46023378209df8253dff5757c752969920
Robert Skelton <robertmskelton@gmail.com>
2014-12-14 23:09:24 -0800

    bats dont change directions properly

--- Assets/Scripts/BatMovement.cs
+++ Assets/Scripts/BatMovement.cs
@@ -1,15 +1,43 @@
-﻿using UnityEngine;
+﻿	using UnityEngine;
 using System.Collections;
 
 public class BatMovement : MonoBehaviour {
 
+	private int speed;
+	private int damage;
+	private int xPrev;	//RS: 1 is up, 2 is left, 3 is down, 4 is right
+	private int yPrev;
+	private int xDir;
+	private int yDir;
 	// Use this for initialization
 	void Start () {
-	
+		speed = 2;
+		damage = 5;
+		xDir = 1;
+		yDir = 1;
+		xPrev = xDir;
+		yPrev = yDir;
 	}
+
+	void ChangeDirection() {
+
+
+	}
+
+	void Move() {
+		transform.position += new Vector3 (xDir * speed, yDir * speed, 0);
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "Player")
+			col.gameObject.BroadcastMessage ("ApplyDamage", damage);
 	
+		ChangeDirection ();
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		Move ();
 	}
 }
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public GameObject bullet; 	// RS: currently our blue circle sprite
+	public float shotCD; 			// RS: can be changed in inspector, may not be the best implementation
+	public float multiplier; 	// RS: is the distance between player and bullet's instantiation
+								// 		must be greater than 0.6						
+	 
+	private Movement player; 	// RS: gets the player's movement script for "facing"
+	private float timestamp;
+	// Use this for initialization
+	void Start () {
+		shotCD = 0.2f;
+		player = GameObject.Find ("Player").GetComponent<Movement> ();
+		timestamp = Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// RS: if the player wants to shoot, we make sure that it has been 
+		//  long enough of a cooldown
+		if(Input.GetKeyDown(KeyCode.Z) && timestamp <= Time.time)
+		{
+			GameObject clone;	
+
+			switch(player.facing)
+			{
+			case 1:
+				clone = (GameObject) Instantiate(bullet, transform.position + multiplier * Vector3.up , Quaternion.identity);
+				break;
+			case 2:
+				clone = (GameObject) Instantiate(bullet, transform.position + multiplier * Vector3.left , Quaternion.identity);
+				break;
+			case 3:
+				clone = (GameObject) Instantiate(bullet, transform.position + multiplier * Vector3.down , Quaternion.identity);
+				break;
+			case 4:
+				clone = (GameObject) Instantiate(bullet, transform.position + multiplier * Vector3.right , Quaternion.identity);
+				break;
+			}
+
+			// RS: means next time player can shoot will be the current time + however long
+			//	the cooldown is
+			timestamp = Time.time + shotCD;
+		}
+	}
+}
