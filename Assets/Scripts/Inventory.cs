b7f2bc600e61021114eaa623f763dec8242c5a9a
jheckard <jason.heckard@yahoo.com>
2014-12-14 20:05:33 -0800

    working on getting key/door working and adding Meerkat camps

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour 
+{
+	public bool key;
+
+	// Use this for initialization
+	void Start () 
+	{
+		key = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
