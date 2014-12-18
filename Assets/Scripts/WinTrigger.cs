e7af4c0d0e24e655a1c83a63929ce156a15f521d
Cheddar2012 <cheddar1@outlook.com>
2014-12-18 00:31:19 -0800

    Added win condition

---
+++ Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinTrigger : MonoBehaviour {
+
+	public GameObject boss;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			if (boss == null) {
+				Application.LoadLevel("Scene003");
+			}
+		}
+	}
+}
