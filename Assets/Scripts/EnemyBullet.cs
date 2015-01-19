9abfc160de3994c034fe42f6131ed3c0039a36b8
jheckard <jason.heckard@yahoo.com>
2015-01-18 19:02:33 -0800

    Made  few small changes; started on PlayerGrenade
    PlayerGrenade is in progress. Just doing a push to keep the repo current

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EnemyBullet : MonoBehaviour {
 	
-	public float damage = 15;
+	public float damage = 10;
 	public float pushForce = 200;
 	
 	public int direction{ get; set; }
---
+++ Assets/Scripts/PlayerGrenade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGrenade : MonoBehaviour 
+{
+	public float damage = 20;
+	public Vector3 speed;
+	private int direction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// direction = GameObject.Find ("Player").GetComponent<Movement> ().facing;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
