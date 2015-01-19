82e4cdb686e9cf7e17dfc0ca42e44ed5c30b62fa
jheckard <jason.heckard@yahoo.com>
2015-01-18 19:39:37 -0800

    Animated Ant Hills Finished!
    It looks awesome!

---
+++ Assets/Scripts/AntHillAnimator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntHillAnimator : MonoBehaviour 
+{
+	// The array of sprites used for animating this character
+	public Sprite[] antHills;
+	
+	// Number of frames to animate each second
+	public int framesPerSecond = 2;
+	
+	// The object's SpriteRenderer
+	public SpriteRenderer antHillRend;
+	
+	// Countdown until the next frame
+	private float frameTimer;
+	
+	// Number of frames in  animation
+	private float antFrames;
+	
+	// Which sprite to show in the sprites array
+	public int spriteToShow;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		antHillRend = (SpriteRenderer)GetComponent("SpriteRenderer");
+		ResetFrameTimer();
+		antHillRend.sprite = antHills[3];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		frameTimer -= Time.deltaTime;
+		if (frameTimer <= 0) 
+		{
+			spriteToShow = (spriteToShow + 1) % 4;
+			ResetFrameTimer();
+		}
+		
+		antHillRend.sprite = antHills[spriteToShow];
+	}
+
+	void ResetFrameTimer() 
+	{
+		frameTimer = 1.0f / (float)framesPerSecond;
+	}
+}
