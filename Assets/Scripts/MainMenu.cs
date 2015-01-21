35a40ee91e14057114e74581928bea721594bf2d
jheckard <jason.heckard@yahoo.com>
2015-01-20 23:01:17 -0800

    First Draft credit screen and buttons
    Button positions need to be fixed, based on ideal game play resolution.
    We need music credits!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -16,9 +16,38 @@ public class MainMenu : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+	void Update () 
+	{
+		/*
+		if (Input.GetMouseButtonDown(0)) 
+		{
+
+		}
+		*/
+	}
+
+	// got the entire onGUI button interaction from Unity API 
+	void OnGUI() 
+	{
+		/*
+		if (!btnTexture) 
+		{
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+		if (GUI.Button(Rect(10,10,50,50),btnTexture))
+			Debug.Log("Clicked the button with an image");
+		*/
+		if(GUI.Button(new Rect(400,400,100,60),"Start") )
+		{
+			// Debug.Log("Clicked the button with text");
 			Application.LoadLevel("Scene002");
+		}
+
+		if(GUI.Button(new Rect(400,500,100,60),"Credits") )
+		{
+			// Debug.Log("Clicked the button with text");
+			Application.LoadLevel("credits");
 		}
 	}
 }
---
+++ Assets/credits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class credits : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(400,500,100,60),"Back") )
+		{
+			// Debug.Log("Clicked the button with text");
+			Application.LoadLevel("Scene001");
+		}	
+	}
+}
